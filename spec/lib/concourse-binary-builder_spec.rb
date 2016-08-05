057db1b85494d7613ac0120a859960ecb4262d19
Sam Smith <sesmith177@gmail.com>
2016-08-05 16:39:25 -0400

    WIP; refactor specs using shared examples; add coverage for python and glide

--- spec/lib/concourse-binary-builder_spec.rb
+++ spec/lib/concourse-binary-builder_spec.rb
@@ -61,16 +61,7 @@ describe ConcourseBinaryBuilder do
       FileUtils.rm_rf(task_root_dir)
     end
 
-
-    context 'the dependency is manually built' do
-      let(:dependency) { 'go' }
-      let(:output_file) { 'go1.6.3.linux-amd64.tar.gz' }
-      let(:source_sha256) { '6326aeed5f86cf18f16d6dc831405614f855e2d416a91fd3fdc334f772345b00' }
-      let(:source_url) { 'https://storage.googleapis.com/golang/go1.6.3.src.tar.gz' }
-      let(:version) { '1.6.3' }
-
-
-      before { subject.run }
+    shared_examples_for 'a commit is made in builds-yaml-artifacts with the proper git message' do |automation|
 
       it 'makes the commit with dependency + version' do
         commit_msg = `cd #{builds_yaml_artifacts_dir} && git log -1 HEAD`
@@ -94,9 +85,57 @@ describe ConcourseBinaryBuilder do
         expect(commit_msg).to include("md5: #{md5sum}")
         expect(commit_msg).to include("sha256: #{shasum}")
       end
+
+      if automation == 'automated'
+        it 'makes the commit with a timestamp' do
+          Dir.chdir(builds_yaml_artifacts_dir) do
+            built_yaml = YAML.load_file("#{dependency}-built.yml")
+            expect(built_yaml[dependency][0]['timestamp']).to_not eq nil
+          end
+        end
+      end
     end
 
-    context 'the dependency is automatically built' do
+
+
+    context 'the dependency is go' do
+      let(:dependency) { 'go' }
+      let(:output_file) { 'go1.6.3.linux-amd64.tar.gz' }
+      let(:source_sha256) { '6326aeed5f86cf18f16d6dc831405614f855e2d416a91fd3fdc334f772345b00' }
+      let(:source_url) { 'https://storage.googleapis.com/golang/go1.6.3.src.tar.gz' }
+      let(:version) { '1.6.3' }
+
+      before { subject.run }
+
+      it_behaves_like 'a commit is made in builds-yaml-artifacts with the proper git message', 'not automated'
+    end
+
+    context 'the dependency is python' do
+      let(:dependency) { 'python' }
+      let(:output_file) { 'python-2.7.12-linux-x64.tgz' }
+      let(:source_sha256) { 'f036b03f2ffd401742bb053f41c25dbe4491e52fc06e49b0dd0e9c1ae5a7baf7' }
+      let(:source_url) { 'https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tgz' }
+      let(:version) { '2.7.12' }
+
+      before { subject.run }
+
+      it_behaves_like 'a commit is made in builds-yaml-artifacts with the proper git message', 'not automated'
+    end
+
+    context 'the dependency is glide' do
+      let(:dependency) { 'glide' }
+      let(:output_file) { 'glide-v0.11.1-linux-x64.tgz' }
+      let(:source_sha256) { '3c4958d1ab9446e3d7b2dc280cd43b84c588d50eb692487bcda950d02b9acc4c' }
+      let(:source_url) { 'https://github.com/Masterminds/glide/archive/v0.11.1.tar.gz' }
+      let(:version) { 'v0.11.1' }
+
+      before { subject.run }
+
+      it_behaves_like 'a commit is made in builds-yaml-artifacts with the proper git message', 'automated'
+    end
+
+
+    context 'the dependency is node' do
       let(:dependency) { 'node' }
       let(:output_file) { 'node-4.4.7-linux-x64.tgz' }
       let(:source_sha256) { 'cbe1c6e421969dd5639d0fbaa6d3c1f56c0463b87efe75be8594638da4d8fc4f' }
@@ -106,36 +145,8 @@ describe ConcourseBinaryBuilder do
 
       before { subject.run }
 
-      it 'makes the commit with dependency + version' do
-        commit_msg = `cd #{builds_yaml_artifacts_dir} && git log -1 HEAD`
-        expect(commit_msg).to include("Build #{dependency} - #{version}")
-      end
+      it_behaves_like 'a commit is made in builds-yaml-artifacts with the proper git message', 'automated'
 
-      it 'makes the commit with source url and sha256' do
-        commit_msg = `cd #{builds_yaml_artifacts_dir} && git log -1 HEAD`
-
-        expect(commit_msg).to include("source sha256: #{source_sha256}")
-        expect(commit_msg).to include("source url: #{source_url}")
-      end
-
-      it 'makes the commit with a timestamp' do
-        Dir.chdir(builds_yaml_artifacts_dir) do
-          built_yaml = YAML.load_file("#{dependency}-built.yml")
-          expect(built_yaml[dependency][0]['timestamp']).to_not eq nil
-        end
-      end
-
-
-      it 'makes the commit with output filename, md5, and sha256' do
-        commit_msg = `cd #{builds_yaml_artifacts_dir} && git log -1 HEAD`
-
-        md5sum = Digest::MD5.file(File.join(binary_builder_dir, output_file)).hexdigest
-        shasum = Digest::SHA256.file(File.join(binary_builder_dir, output_file)).hexdigest
-
-        expect(commit_msg).to include(output_file)
-        expect(commit_msg).to include("md5: #{md5sum}")
-        expect(commit_msg).to include("sha256: #{shasum}")
-      end
     end
 
     context 'the dependency is composer' do
@@ -148,38 +159,8 @@ describe ConcourseBinaryBuilder do
 
       before { subject.run }
 
-      it 'makes the commit with dependency + version' do
-        commit_msg = `cd #{builds_yaml_artifacts_dir} && git log -1 HEAD`
-        expect(commit_msg).to include("Build #{dependency} - #{version}")
-      end
+      it_behaves_like 'a commit is made in builds-yaml-artifacts with the proper git message', 'automated'
 
-      it 'makes the commit with source url and sha256' do
-        commit_msg = `cd #{builds_yaml_artifacts_dir} && git log -1 HEAD`
-
-        expect(commit_msg).to include("source sha256: #{source_sha256}")
-        expect(commit_msg).to include("source url: #{source_url}")
-      end
-
-      it 'makes the commit with a timestamp' do
-        Dir.chdir(builds_yaml_artifacts_dir) do
-          built_yaml = YAML.load_file("#{dependency}-built.yml")
-          expect(built_yaml[dependency][0]['timestamp']).to_not eq nil
-        end
-      end
-
-      it 'makes the commit with output filename, md5, and sha256' do
-        commit_msg = `cd #{builds_yaml_artifacts_dir} && git log -1 HEAD`
-
-        md5sum = Digest::MD5.file(File.join(binary_builder_dir, output_file)).hexdigest
-        shasum = Digest::SHA256.file(File.join(binary_builder_dir, output_file)).hexdigest
-
-        expect(commit_msg).to include(output_file)
-        expect(commit_msg).to include("md5: #{md5sum}")
-        expect(commit_msg).to include("sha256: #{shasum}")
-      end
     end
-
-
-
   end
 end
