082a4ee73f0ffac11a3fb1c569f30572e6b8b17a
Sam Smith <sesmith177@gmail.com>
2016-08-05 15:04:24 -0400

    WIP; add initial specs for concourse-binary-builder

--- lib/concourse-binary-builder.rb
+++ lib/concourse-binary-builder.rb
@@ -58,25 +58,20 @@ class ConcourseBinaryBuilder
         flags << %( --#{key}="#{value}")
       end
 
-      Dir.chdir(binary_builder_dir) do
-        @binary_builder_output = `./bin/binary-builder #{flags}`
-        raise "Could not build" unless $?.success?
-        if Dir.exist?("/tmp/x86_64-linux-gnu/")
-          system('tar -zcf build.tgz -C /tmp ./x86_64-linux-gnu/') or raise "Could not create tar"
-        elsif Dir.exist?("/tmp/src/")
-          # godep and glide are written in Go, so their source is downloaded to
-          # "/tmp/src"
-          system('tar -zcf build.tgz -C /tmp ./src/') or raise "Could not create tar"
-        else
-          raise "Could not find original source after build"
-        end
-      end
-      FileUtils.cp_r(Dir["binary-builder/*.tgz", "binary-builder/*.tar.gz"], "binary-builder-artifacts/")
-      FileUtils.mkdir_p('binary-builder-artifacts/final-artifact/')
-      FileUtils.cp_r('binary-builder-artifacts/build.tgz', 'binary-builder-artifacts/final-artifact/')
+
+      @binary_builder_output = run_binary_builder(flags)
+
+
+      binary_artifacts_dir = File.join(task_root_dir, 'binary-builder-artifacts')
+      binary_final_artifacts_dir = File.join(binary_artifacts_dir, 'final-artifact')
+      FileUtils.mkdir_p(binary_final_artifacts_dir)
+
+      FileUtils.cp_r(Dir["#{binary_builder_dir}/*.tgz", "#{binary_builder_dir}/*.tar.gz"], binary_artifacts_dir)
+      FileUtils.cp_r("#{binary_artifacts_dir}/build.tgz", binary_final_artifacts_dir)
     end
 
-    /^Source URL:\s(.*)$/.match(@binary_builder_output)
+
+    /- url:\s(.*)$/.match(@binary_builder_output)
     source_url = $1
 
     ext = case binary_name
@@ -88,11 +83,15 @@ class ConcourseBinaryBuilder
               '-*.tgz'
           end
 
-    filename = Dir["binary-builder/#{binary_name + ext}"].first
+    filename = Dir["#{binary_builder_dir}/#{binary_name + ext}"].first
+    filename.match /(binary-builder\/.*)/
+    short_filename = $1
+
     md5sum = Digest::MD5.file(filename).hexdigest
     shasum = Digest::SHA256.file(filename).hexdigest
-    git_msg = "Build #{binary_name} - #{latest_build['version']}\n\nfilename: #{filename}, md5: #{md5sum}, sha256: #{shasum}"
-    git_msg += "\n\nsource url: #{source_url}, #{@verification_type}: #{@verification_value}"
+
+    git_msg = "Build #{binary_name} - #{latest_build['version']}\n\nfilename: #{short_filename}, md5: #{md5sum}, sha256: #{shasum}"
+    git_msg += "\n\nsource url: #{source_url}, source #{@verification_type}: #{@verification_value}"
     git_msg += "\n\n[ci skip]" if builds[binary_name].empty? && ci_skip_for(binary_name)
 
 
@@ -106,6 +105,27 @@ class ConcourseBinaryBuilder
       commit_and_rsync(builds_dir, builds_yaml_artifacts, git_msg, builds_file)
     end
   end
+
+  private
+
+  def run_binary_builder(flags)
+    Dir.chdir(binary_builder_dir) do
+      output = `./bin/binary-builder #{flags}`
+      raise "Could not build" unless $?.success?
+      if Dir.exist?("/tmp/x86_64-linux-gnu/")
+        system('tar -zcf build.tgz -C /tmp ./x86_64-linux-gnu/') or raise "Could not create tar"
+      elsif Dir.exist?("/tmp/src/")
+        # godep and glide are written in Go, so their source is downloaded to
+        # "/tmp/src"
+        system('tar -zcf build.tgz -C /tmp ./src/') or raise "Could not create tar"
+      else
+        raise "Could not find original source after build"
+      end
+    end
+
+    output
+  end
+
 
   def add_ssh_key_and_update(dir, branch)
     File.write("/tmp/git_ssh_key", git_ssh_key)
@@ -136,7 +156,7 @@ class ConcourseBinaryBuilder
 
   def commit_and_rsync(in_dir, out_dir, git_msg, files)
     Dir.chdir(in_dir) do
-      exec(<<-EOF)
+      system(<<-EOF)
       git config --global user.email "cf-buildpacks-eng@pivotal.io"
       git config --global user.name "CF Buildpacks Team CI Server"
       git add #{files}
