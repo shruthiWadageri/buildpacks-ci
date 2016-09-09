909be74d0d3616c2d7028d2e12627504d706ed93
Jacques Chester <jchester@pivotal.io>
2016-09-09 14:56:26 -0400

    WIP Add chattier output

--- tasks/run-shellcheck/run.rb
+++ tasks/run-shellcheck/run.rb
@@ -3,7 +3,13 @@
 require_relative '../../lib/shell-checker'
 
 directory_to_inspect = ENV['DIRECTORY_TO_CHECK']
+
+puts "Checking #{directory_to_inspect}"
+
 shell_checker = ShellChecker.new.check_shell_files(directory: directory_to_inspect)
+
+puts "Found #{shell_checker.size} shell files to inspect"
+
 shell_checker.each do |file_path, shellcheck_output|
   puts '########################################################################################'
   puts "## shellcheck results for #{file_path}"
