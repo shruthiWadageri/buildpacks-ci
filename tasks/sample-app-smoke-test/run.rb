fe73388219ad230de8507fc892197d9be339b783
Sam Smith <sesmith177@gmail.com>
2016-09-12 17:57:59 -0400

    WIP; use development space for initial login

--- tasks/sample-app-smoke-test/run.rb
+++ tasks/sample-app-smoke-test/run.rb
@@ -3,8 +3,8 @@
 require 'net/http'
 require 'json'
 
-def delete_space
-  cf_delete_space_command = "cf delete-space #{cf_space} -f"
+def delete_space(space)
+  cf_delete_space_command = "cf delete-space #{space} -f"
   cf_delete_space_output = `#{cf_delete_space_command}`
   puts cf_delete_space_output
 end
@@ -14,7 +14,8 @@ cf_username = ENV['CF_USERNAME']
 cf_password = ENV['CF_PASSWORD']
 cf_organization = ENV['CF_ORGANIZATION']
 cf_domain = ENV['CF_DOMAIN']
-cf_space = "sample-app-#{Random.rand(100000)}"
+cf_login_space = ENV ['CF_LOGIN_SPACE']
+cf_app_space = "sample-app-#{Random.rand(100000)}"
 app_name = ENV['APPLICATION_NAME']
 buildpack_url = ENV['BUILDPACK_URL']
 path_to_get = ENV['PATH_TO_GET']
@@ -24,16 +25,16 @@ if cf_api.nil? || cf_username.nil? || cf_password.nil? || cf_organization.nil?
   exit 1
 end
 
-cf_login_command = "cf login -a #{cf_api} -u #{cf_username} -p #{cf_password} -o #{cf_organization}"
+cf_login_command = "cf login -a #{cf_api} -u #{cf_username} -p #{cf_password} -o #{cf_organization} -s #{cf_login_space}"
 cf_login_output = `#{cf_login_command}`
 puts cf_login_output
 
 
-cf_create_space_command = "cf create-space #{cf_space}"
+cf_create_space_command = "cf create-space #{cf_app_space}"
 cf_create_space_output = `#{cf_create_space_command}`
 puts cf_create_space_output
 
-cf_target_command = "cf target -o #{cf_organization} -s #{cf_space}"
+cf_target_command = "cf target -o #{cf_organization} -s #{cf_app_space}"
 cf_target_output = `#{cf_target_command}`
 puts cf_target_output
 
@@ -56,9 +57,9 @@ response = Net::HTTP.get_response("#{host}.#{cf_domain}",path_to_get)
 
 if response.code == "200"
   puts 'Got HTTP response 200. App push successful'
-  delete_space
+  delete_space(cf_app_space)
 else
   puts "Got HTTP response #{response.code}. App push unsuccessful"
-  delete_space
+  delete_space(cf_app_space)
   exit 1
 end
